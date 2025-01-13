e6b4686e71e6c2dfd5161db7882436baaf9c6adb
Amnaa903 <amnasaleem270@gmail.com>
2025-01-13 18:30:13 +0500

    Add files via upload

---
+++ clss 2 js calculator/index.js
@@ -0,0 +1,10 @@
+function abc(){
+let stdTm_grade10 =  document.getElementById("tm1").value;
+alert(stdTm_grade10)
+document.write("stdTm_grade10")
+let stdobt_grade10 =  document.getElementById("obt1").value;
+let = stdobt_grade10/stdTm_grade10 * 100
+document.getElementById("bb").innerHTML=("yougot it"+ res +"%")
+alert.write("you got it"+ res +"%")
+console.com("you got it"+ res +"%")
+}
