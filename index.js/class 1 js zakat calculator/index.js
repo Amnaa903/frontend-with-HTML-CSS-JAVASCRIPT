628d44d5a8be49a9fcfe15bfe718980fdf7e8b17
Amnaa903 <amnasaleem270@gmail.com>
2025-01-14 12:17:36 +0500

    Add files via upload

---
+++ index.js/class 1 js zakat calculator/index.js
@@ -0,0 +1,27 @@
+alert("hi amna");
+alert("9888976987")
+alert(true)
+ savings = prompt("Enter your savings (in your local currency)"); 
+  zakatPercentage = 2.5; 
+ //Percentage for Zakat is usually 2.5%
+zakat = (savings * zakatPercentage) / 100;
+ alert("Your Zakat amount is: " + zakat);
+ document.write("Zakat Amount: " + zakat);
+ weight = prompt("Enter your weight in kilograms");
+height = prompt("Enter your height in meters");
+ bmi = weight / (height * height); 
+ //Formula: BMI = weight / (height^2)
+ alert("Your BMI is: " + bmi);
+document.write("BMI: " + bmi);
+//fxfxfxd
+a = prompt("Enter your name")
+alert(a)
+document.write(a)
+stdoptmarks = prompt("Enter Std opt marks")
+stdtmarks= prompt("Enter std tmarks")
+res = stdoptmarks/stdtmarks*100
+alert(res)
+document.write(res)
+
+
+//
